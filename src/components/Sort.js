e8f44ba18bc0467cdf4460ecb34604f88d338b9f
Pankaj <pankajkumbhare111@gmail.com>
2022-10-21 13:39:13 +0530

    clickable button code

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filtercontext";
+import { BsFillGridFill, BsList } from "react-icons/bs";
 
 const Sort = () => {
+  const { filter_products, grid_view, setGridView, setListView } =
+    useFilterContext();
   return (
-    <div>
-      sort
-    </div>
-  )
-}
+    <Wrapper className="sort-section">
+      <div className="sorting-list--grid">
+        <button className={grid_view ? "active sort-btn" : "sort-btn"} onClick={setGridView}>
+          <BsFillGridFill className="icon" />
+        </button>
+        <button className={!grid_view ? "active sort-btn" : "sort-btn"} onClick={setListView}>
+          <BsList className="icon" />
+        </button>
+      </div>
 
-export default Sort
+      <div className="product-data">
+        <p>{`${filter_products.length} Products`}</p>
+      </div>
+
+      <div className="sort-selection">
+        sorting
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+  .sorting-list--grid {
+    display: flex;
+    gap: 2rem;
+    .sort-btn {
+      padding: 0.8rem 1rem;
+      border: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
+    .icon {
+      font-size: 1.6rem;
+    }
+    .active {
+      background-color: black;
+      color: #fff;
+    }
+  }
+  .sort-selection .sort-selection--style {
+    padding: 0.8rem;
+    cursor: pointer;
+    .sort-select--option {
+      padding: 0.5rem 0;
+      cursor: pointer;
+      height: 2rem;
+      padding: 10px;
+    }
+  }
+`;
+
+export default Sort;
--- src/context/filtercontext.js
+++ src/context/filtercontext.js
@@ -7,7 +7,7 @@ const FilterContext = createContext();
 const initialState = {
     filter_products: [],
     all_products: [],
-    grid_view: true,
+    grid_view: false,
 }
 
 const FilterProvider = ({children}) => {
@@ -15,14 +15,18 @@ const FilterProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const setGridView = () => {
-        return dispatch({type: "SET_GRIDVIEW"});
+        return dispatch({type: "SET_GRID_VIEW"});
+    }
+
+    const setListView = () => {
+        return dispatch({type: "SET_LIST_VIEW"});
     }
 
     useEffect(() => {
         dispatch({type: "SET_FILTER_PRODUCTS", payload: products})
     }, [products]);
 
-    return <FilterContext.Provider value={{...state, setGridView}}>{children}</FilterContext.Provider>
+    return <FilterContext.Provider value={{...state, setGridView, setListView}}>{children}</FilterContext.Provider>
 };
 
 const useFilterContext = () => {
