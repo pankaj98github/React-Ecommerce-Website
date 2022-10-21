20b4946cc3994444423552dfa811efb61a013c67
Pankaj <pankajkumbhare111@gmail.com>
2022-10-21 12:44:05 +0530

    grid view and list view code pushed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {useFilterContext} from "../context/filtercontext";
+import GridView from './GridView';
+import ListView from './ListView';
 
 const ProductList = () => {
-  return (
-    <div>
-      productlist
-    </div>
-  )
-}
+  const { filter_products, grid_view } = useFilterContext();
+
+  if(grid_view === true){
+    return <GridView products={filter_products}/> 
+  }
+
+  if(grid_view === false){
+    return <ListView products={filter_products}/> 
+  }
+};
 
 export default ProductList;
--- src/context/filtercontext.js
+++ src/context/filtercontext.js
@@ -7,17 +7,22 @@ const FilterContext = createContext();
 const initialState = {
     filter_products: [],
     all_products: [],
+    grid_view: true,
 }
 
 const FilterProvider = ({children}) => {
     const {products} = useProductContext();
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const setGridView = () => {
+        return dispatch({type: "SET_GRIDVIEW"});
+    }
+
     useEffect(() => {
         dispatch({type: "SET_FILTER_PRODUCTS", payload: products})
     }, [products]);
 
-    return <FilterContext.Provider value={{...state}}>{children}</FilterContext.Provider>
+    return <FilterContext.Provider value={{...state, setGridView}}>{children}</FilterContext.Provider>
 };
 
 const useFilterContext = () => {
